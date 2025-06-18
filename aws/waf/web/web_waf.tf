e104810eecc3d85b8ee9a39d7cb123a0a023e505
Jonathan Case <jcase@Jonathans-MacBook-Air.local>
2025-06-18 18:24:08 -0400

    initial waf config

---
+++ aws/waf/web/web_waf.tf
@@ -0,0 +1,214 @@
+resource "aws_wafv2_web_acl" "web_acl" {
+  name        = "web-waf-acl"
+  scope       = "REGIONAL"
+  description = "Comprehensive WAF ACL for web traffic with AWS Managed and custom rules"
+
+  default_action {
+    allow {
+  }
+}
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "webWAF"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "GeoBlockNonUS"
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      not_statement {
+        statement {
+          geo_match_statement {
+            country_codes = ["US"]
+          }
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "GeoBlockNonUS"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "RateLimitPerIP"
+    priority = 2
+    action {
+      block {}
+    }
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "RateLimitPerIP"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "BlockMissingUserAgent"
+    priority = 3
+    action {
+      block {}
+    }
+    statement {
+      byte_match_statement {
+        field_to_match {
+          
+headers {
+  match_scope       = "ALL"
+  oversize_handling = "MATCH"
+  match_pattern {
+    included_headers = ["User-Agent"]
+  }
+}
+
+        }
+        positional_constraint = "EXACTLY"
+        search_string = "Mozilla/"
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "MissingUserAgent"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "BlockPHPFiles"
+    priority = 4
+    action {
+      block {}
+    }
+    statement {
+      byte_match_statement {
+        field_to_match {
+          uri_path {}
+        }
+        positional_constraint = "ENDS_WITH"
+        search_string         = ".php"
+        text_transformation {
+          priority = 0
+          type     = "NONE"
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "BlockPHP"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 5
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 6
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "KnownBadInputs"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesSQLiRuleSet"
+    priority = 7
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesSQLiRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "SQLiRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesAnonymousIpList"
+    priority = 8
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAnonymousIpList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AnonymousIpList"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "AWSManagedRulesAmazonIpReputationList"
+    priority = 9
+    override_action {
+      none {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "IPReputation"
+      sampled_requests_enabled   = true
+    }
+  }
+
+}
+
