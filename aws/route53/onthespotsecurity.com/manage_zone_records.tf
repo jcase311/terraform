2ea654549eb2c40f93a3cf10695c4c3163c1e7a2
Jonathan Case <jcase@Jonathans-MacBook-Air.local>
2025-06-18 16:43:40 -0400

    updated with new web app cnames

--- aws/route53/onthespotsecurity.com/manage_zone_records.tf
+++ aws/route53/onthespotsecurity.com/manage_zone_records.tf
@@ -1,11 +1,19 @@
 #this is where you will add additional dns entries 
 #records of A,CNAME,MX etc..
 
+resource "aws_route53_record" "web" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "www.onthespotsecurity.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["web-alb-1999023055.us-east-2.elb.amazonaws.com"]
 
-resource "aws_route53_record" "app_ip" {
+}
+resource "aws_route53_record" "app" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "app.onthespotsecurity.com"
   type    = "CNAME"
   ttl     = 300
-  records = ["cis-alb-1800985296.us-east-2.elb.amazonaws.com"]  # replace with your actual public IP
+  records = ["internal-app-alb-293253736.us-east-2.elb.amazonaws.com"]
+
 }
