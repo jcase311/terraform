f3a79b4c5d47223989d07e5471604c2bd9ed87d6
jcase311 <jcase311@users.noreply.github.com>
2025-07-17 11:29:43 -0400

    initial vpc configs

---
+++ aws/vpc/variables.tf
@@ -0,0 +1,36 @@
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "aws-secure-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-2a", "us-east-2b"]
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "database_subnets" {
+  description = "List of database subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.21.0/24", "10.0.22.0/24"]
+}
